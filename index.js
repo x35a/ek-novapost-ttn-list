0cc1d95a8b69ad84a2d6a02f7d9d667b666342bf
xj <8172473+x35a@users.noreply.github.com>
2024-11-27 00:39:20 +0200

    add warn info for backpayment delay

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function fetchTTNList(url, data) {
 
       resultString += `${removeTime(item.RecipientDateTime)} - ${
         afterPayment || `${backwardDelivery} cash`
-      } - ${item.IntDocNumber}\n`;
+      }${/Пункт приймання-видачі/i.test(item.RecipientAddressDescription) ? ' delay' : ''} - ${item.IntDocNumber}\n`;
     }
 
     return resultString;
