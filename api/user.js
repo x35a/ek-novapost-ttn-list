47d73f64fbfcd886bb50b9fe41d87fed5bce5ed2
xj <xj@email.com>
2025-04-19 15:59:59 +0300

    add test file

---
+++ api/user.js
@@ -0,0 +1,7 @@
+// api/user.js
+export async function GET(request) {
+    const url = new URL(request.url);
+    const name = url.searchParams.get('name') || 'World';
+  
+    return Response.json({ message: `Hello ${name}!` });
+  }
